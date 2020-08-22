11f40f8bac2408af362b549a567c5694ba79ca4c
bxchang04 <bxchang04@gmail.com>
2020-08-22 16:49:51 -0400

    first commit

---
+++ levels/Level 0.js
@@ -0,0 +1,12 @@
+Level 0
+
+// Indentation
+  // 1 line
+  // Multi line
+  // Staggered line
+  
+// Alt+Shift+F
+
+// Tab
+
+  // Editor left right
---
+++ levels/Level 1.js
@@ -0,0 +1,68 @@
+Level 1a
+
+// 1 line
+// remove brackets
+// add brackets
+
+// fix missing bracket
+// fix extra bracket
+
+// nested brackets
+
+[ arstar rast ar arstar taart ars tarst arssra ]
+
+Level 1b
+// repeat for each type of bracket
+
+Level 1c
+// repeat for 3 types of quotes ' " `
+
+
+
+// Linear or can choose?
+  Linear at first but then choose
+
+
+
+Types of Levels / Skill to Learn
+
+  Selection, cut, paste
+    jumping from home to end
+    entire line
+    partial line 
+
+  needle in haystack
+    single char
+    words
+    special chars
+    combination
+
+  diffing
+    eyeball
+    editor
+
+  Finding duplicates and typos of duplicates
+
+  Looking up function definitions and tracing things to their ShadowRoot
+
+  Finding initializations, declarations, both.Finding
+
+  Object notation
+
+  Flow Control
+    if, else, if else
+    switch case
+    continue, break, return
+    labeled blocks
+
+  Loops
+    for, forEach, for in, for of
+    while, do while
+
+  HTML 
+
+  CSS 
+
+  SQL 
+
+  Java 
