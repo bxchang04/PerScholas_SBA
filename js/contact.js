7fcb77358c43475e4e1507ab8043adfe9158c08a
bxchang04 <bxchang04@gmail.com>
2020-08-23 13:38:37 -0400

    implement css animation and contact form

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,2 @@
-console.log("contact js loaded");
-
-// HTML should be onclick. not onClick!!
-function promptTY(e) {
-  e.preventDefault(); // in order for this to work, e must be passed into the inlineEventHandlerAttribute as a parameter! AND, element must be button and NOT input.
-
-  console.log("TY!");
-}
+// note to self: HTML should be onclick. not onClick!!
+console.log("contact js is loaded");
