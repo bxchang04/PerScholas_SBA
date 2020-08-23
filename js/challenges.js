a96f63a9249e5b6f2b9be7f5aaccf8d6736d854c
bxchang04 <bxchang04@gmail.com>
2020-08-23 14:54:19 -0400

    implement challenges wip

---
+++ js/challenges.js
@@ -0,0 +1,30 @@
+/* 
+
+make table
+make input fields
+
+make submit button
+
+onsubmit, reveal answers
+
+try again button, set values of input fields  and answers to ''
+
+
+quiz:
+
+according to the home page of this site, the first important skill for coders is:
+
+
+according to the home page of this site, the second important skill for coders is:
+
+
+according to the home page of this site, the third important skill for coders is:
+
+  submit
+
+results
+
+  try again
+
+*/
+
